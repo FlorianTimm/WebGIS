829a864f840193a4fdeb0931c10dff6daf306900
Florian Timm <flotimm@gmail.com>
2022-09-27 21:03:02 +0200

    Klassendiagramm

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,33 +1,3 @@
-//import "../js/import_jquery.js";
-import '../../node_modules/ol/ol.css'
-import '../style/index.css';
-import Map from "./openLayers/Map";
-import Hauptmenu from "./gui/Hauptmenu";
-import { Navigation } from "./gui/Navigation";
-import { LayerMenu } from "./gui/LayerMenu";
-import KameraMenu from "./gui/KameraMenu";
-import GenehmigungMenu from "./gui/GenehmigungMenu";
+import UAVplaner from "./UAVplaner";
 
-let map = new Map();
-
-createMenu();
-
-function createMenu() {
-    let navigation = new Navigation(document.getElementsByTagName('nav')[0], document.getElementsByTagName('aside')[0]);
-
-    // Hauptmenü
-    let hm = new Hauptmenu(map);
-    navigation.appendMenu(hm);
-
-    // Layer
-    let lm = new LayerMenu(map);
-    navigation.appendMenu(lm);
-
-    // UAV-Auswahl
-    let um = new KameraMenu();
-    navigation.appendMenu(um);
-
-    // Genehmigung
-    let gn = new GenehmigungMenu(map, hm.zeichnen);
-    navigation.appendMenu(gn);
-}
+new UAVplaner()
